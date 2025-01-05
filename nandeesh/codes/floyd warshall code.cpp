b659c079a9174f97bf4b17af420a92196bc71cb3
Nandeesh_IB <146099757+nandeeshib@users.noreply.github.com>
2025-01-05 19:10:34 +0530

    Create floyd warshall code.cpp

---
+++ nandeesh/codes/floyd warshall code.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int INF = 1e9;
+
+void floydWarshall(vector<vector<int>> &graph) {
+    int n = graph.size();
+    vector<vector<int>> dist = graph;
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    cout << "Shortest distances between every pair of vertices:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> graph = {
+        {0, 3, INF, INF},
+        {2, 0, INF, INF},
+        {INF, 7, 0, 1},
+        {6, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
